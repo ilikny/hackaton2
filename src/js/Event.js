769724b1f51a7fc6c1c82b359c80fd0634e17d84
Ephram Daniels <linguistnick7@gmail.com>
2023-09-29 13:28:31 +0200

    modular window function

--- src/js/Event.js
+++ src/js/Event.js
@@ -3,9 +3,15 @@ export class Event {
     this.element = document.createElement("div");
     this.parent = parent;
     this.data = data;
+    this.init();
+  }
+
+  init() {
     this.createElement();
     this.openForm();
+    this.closeModalButton();
   }
+
   createElement() {
     this.element.innerHTML = `
     <div class="box__event">
@@ -21,13 +27,23 @@ export class Event {
 
   // the below function will open the modal pop-up window:
   openForm() {
+    const data = this.data;
+    const modalContent = document.getElementById("modalContent");
+    modalContent.innerHTML = `<div class="eventLeft__formRight"><div class="event__thumbnail"><img src="data.image_url" alt="${data.name}"></div>
+    <div class="event__info"><h4>${data.name}</h4><p class="event__date">${data.date}</p></div><div class="form__div"></div><span class="close">&times;</span></div>`;
+  }
+
+  closeModalButton() {
     const popupButton = this.element.querySelector(".loadData");
+    const span = document.querySelector(".close");
     const modal = document.getElementById("myModal");
-    const span = document.getElementsByClassName("close")[0];
+    
     popupButton.onclick = function () {
       modal.style.display = "block";
+
     };
     span.onclick = function () {
+      console.log("clicked");
       modal.style.display = "none";
     };
     window.onclick = function (event) {
@@ -37,3 +53,10 @@ export class Event {
     };
   }
 }
+
+// Paths: (ALWAYS use it inside ${})
+// Name: data.name
+// Description: data.description
+// Date: data.date
+// Image URL: data.image_url
+// In case we need ID (starts with 1 as opposed to index): data.id
