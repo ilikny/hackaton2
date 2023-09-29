52a9e101e456c55287a9523dffd0db044255d770
Ilona Kny <ili@gonuts.cz>
2023-09-29 14:29:42 +0200

    fix fuck ups

--- src/js/Event.js
+++ src/js/Event.js
@@ -1,6 +1,7 @@
 export class Event {
   constructor(parent, data) {
     this.element = document.createElement("div");
+    // this.openWindowThing = document.createElement("div")
     this.parent = parent;
     this.data = data;
     this.createElement();
@@ -14,19 +15,30 @@ export class Event {
     </div>
     `;
   }
-
+  displayModal(modal) {
+      modal.style.display = "block";
+      console.log(this)
+      modal.innerHTML = `hello ${this.data.name}`;
+      // this.openWindowThing.innerHTML = `hello`
+  }
   appendSelf() {
     this.parent.appendChild(this.element);
   }
-
   // the below function will open the modal pop-up window:
   openForm() {
     const popupButton = this.element.querySelector(".loadData");
     const modal = document.getElementById("myModal");
     const span = document.getElementsByClassName("close")[0];
-    popupButton.onclick = function () {
-      modal.style.display = "block";
-    };
+    // modal.appendChild(this.openWindowThing)
+    popupButton.addEventListener('click', () => {
+      this.displayModal(modal)
+    });
+  //   popupButton.addEventListener('click', function() {
+  //     modal.style.display = "block";
+  //     console.log(this)
+  //     modal.innerHTML = `hello ${this.data.name}`;
+  //     // this.openWindowThing.innerHTML = `hello`
+  // });
     span.onclick = function () {
       modal.style.display = "none";
     };
@@ -37,3 +49,35 @@ export class Event {
     };
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Message Daniel, NRDaniels
+
+
+
+
+
+
+
+
+
+// Return to add a new line
+// mobile icon
+// Stay up to date from anywhere with the Slack mobile app
+
+
+
